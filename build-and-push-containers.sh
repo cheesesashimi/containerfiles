f1b6c949ca802d7accdfd0849d7c4bc2c0d5cec0
Zack Zlotnik <zackzlotnik@gmail.com>
2024-04-17 01:38:41 -0400

    add git rev, repo, and name labels to all images

--- build-and-push-containers.sh
+++ build-and-push-containers.sh
@@ -7,28 +7,48 @@ creds_path="$1"
 podman pull registry.fedoraproject.org/fedora-toolbox:39
 podman pull registry.fedoraproject.org/fedora:latest
 
-podman build --build-arg-file=<(./get-versions-for-build-args.sh) -t quay.io/zzlotnik/toolbox:tools --file=./toolbox/Containerfile.tools .
-
 declare -rA images=(
+  ["./toolbox/Containerfile.tools"]="quay.io/zzlotnik/toolbox:tools"
   ["./toolbox/Containerfile.base"]="quay.io/zzlotnik/toolbox:base"
   ["./toolbox/Containerfile.neovim"]="quay.io/zzlotnik/toolbox:neovim"
   ["./ocp-ssh-debug/Containerfile"]="quay.io/zzlotnik/testing:ssh-debug-pod"
   ["./silverblue/Containerfile"]="quay.io/zzlotnik/toolbox:silverblue"
 )
 
-containerfiles=(
+containerfiles_to_build=(
+  ./toolbox/Containerfile.tools
   ./toolbox/Containerfile.base
   ./toolbox/Containerfile.neovim
   ./ocp-ssh-debug/Containerfile
   ./silverblue/Containerfile
 )
 
-for containerfile in "${containerfiles[@]}"; do
+containerfiles_to_push=(
+  ./toolbox/Containerfile.base
+  ./toolbox/Containerfile.neovim
+  ./ocp-ssh-debug/Containerfile
+  ./silverblue/Containerfile
+)
+
+build_arg_file="$(mktemp -d)/build-args"
+./get-versions-for-build-args.sh > "$build_arg_file"
+echo "GIT_REVISION=$(git rev-parse HEAD)" >> "$build_arg_file"
+echo "GIT_REPO=https://github.com/cheesesashimi/containerfiles" >> "$build_arg_file"
+
+for containerfile in "${containerfiles_to_build[@]}"; do
   tag="${images[${containerfile}]}"
-  podman build -t "$tag" --file "$containerfile" .
+  cleaned_containerfile_path="$(echo "$containerfile" | sed 's/\.\///g')"
+  podman build \
+    --tag "$tag" \
+    --file "$containerfile" \
+    --build-arg-file="$build_arg_file" \
+    --build-arg=CONTAINERFILE_NAME="$cleaned_containerfile_path" \
+      .
 done
 
-for containerfile in "${containerfiles[@]}"; do
+for containerfile in "${containerfiles_to_push[@]}"; do
   tag="${images[${containerfile}]}"
   podman push --authfile="$creds_path" "$tag"
 done
+
+rm -rf "$(dirname "$build_arg_file")"
