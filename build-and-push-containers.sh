102f26b34467c3f0ae02f7488c146a3a515bc7e9
Zack Zlotnik <zackzlotnik@gmail.com>
2024-04-17 01:50:17 -0400

    adds a fedora coreos image

--- build-and-push-containers.sh
+++ build-and-push-containers.sh
@@ -4,14 +4,19 @@ set -xeuo
 
 creds_path="$1"
 
-podman pull registry.fedoraproject.org/fedora-toolbox:39
-podman pull registry.fedoraproject.org/fedora:latest
+base_images=(
+  registry.fedoraproject.org/fedora-toolbox:39
+  registry.fedoraproject.org/fedora:latest
+  quay.io/fedora/fedora-coreos:stable
+  quay.io/fedora/fedora-silverblue:39
+)
 
 declare -rA images=(
   ["./toolbox/Containerfile.tools"]="quay.io/zzlotnik/toolbox:tools"
   ["./toolbox/Containerfile.base"]="quay.io/zzlotnik/toolbox:base"
   ["./toolbox/Containerfile.neovim"]="quay.io/zzlotnik/toolbox:neovim"
   ["./ocp-ssh-debug/Containerfile"]="quay.io/zzlotnik/testing:ssh-debug-pod"
+  ["./fedora-coreos/Containerfile"]="quay.io/zzlotnik/toolbox:fedora-coreos"
   ["./silverblue/Containerfile"]="quay.io/zzlotnik/toolbox:silverblue"
 )
 
@@ -20,6 +25,7 @@ containerfiles_to_build=(
   ./toolbox/Containerfile.base
   ./toolbox/Containerfile.neovim
   ./ocp-ssh-debug/Containerfile
+  ./fedora-coreos/Containerfile
   ./silverblue/Containerfile
 )
 
@@ -27,8 +33,13 @@ containerfiles_to_push=(
   ./toolbox/Containerfile.base
   ./toolbox/Containerfile.neovim
   ./ocp-ssh-debug/Containerfile
+  ./fedora-coreos/Containerfile
   ./silverblue/Containerfile
 )
+
+for base_image in "${base_images[@]}"; do
+  podman pull "$base_image"
+done
 
 build_arg_file="$(mktemp -d)/build-args"
 ./get-versions-for-build-args.sh > "$build_arg_file"
