02aea52d31db6d27cad853e8e1c329c5d335a17d
Zack Zlotnik <zackzlotnik@gmail.com>
2025-10-02 14:37:07 -0400

    make pathing more consistent

--- tools-fetcher/install.sh
+++ tools-fetcher/install.sh
@@ -17,17 +17,22 @@ repos=(
   "wagoodman/dive"
 )
 
-mkdir -p /yq
-dra download -a mikefarah/yq -o /yq/yq --install-file "yq_linux_$(get-arch --amd64 --arm64)"
+root_path="/out"
+
+mkdir -p "$root_path/yq"
+dra download -a mikefarah/yq -o "$root_path/yq/yq" --install-file "yq_linux_$(get-arch --amd64 --arm64)"
 
 for repo in "${repos[@]}"; do
   name="$(basename "$repo")"
-  mkdir -p "/$name"
-  dra download -a "$repo" -o "/$name/$name" --install-file "$name"
+  mkdir -p "$root_path/$name"
+  dra download -a "$repo" -o "$root_path/$name/$name" --install-file "$name"
 done
 
-mv /ignition/ignition /ignition/ignition-validate
+mv "$root_path/ignition/ignition" "$root_path/ignition/ignition-validate"
 
-mkdir -p /oc
-cd /oc
+mkdir -p "$root_path/oc"
+cd "$root_path/oc"
 curl -L "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable/openshift-client-linux$(get-arch --custom-x86_64="" --custom-aarch64='-arm64')".tar.gz | tar xz
+find "$root_path" -type f -executable -exec mv {} "/usr/local/bin/" \;
+rm -rf "$root_path"
+rm /usr/local/bin/get-arch
