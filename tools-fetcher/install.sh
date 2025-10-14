21d9ee4ba2694678c037effda938491d805c549a
Zack Zlotnik <zackzlotnik@gmail.com>
2025-10-14 17:37:40 -0400

    add jira-cli to workspace image

--- tools-fetcher/install.sh
+++ tools-fetcher/install.sh
@@ -2,33 +2,43 @@
 
 set -xeuo
 
-repos=(
-  "block/goose"
-  "coreos/ignition"
-  "coreos/butane"
-  "derailed/k9s"
-  "ericchiang/pup"
-  "getantibody/antibody"
-  "gmeghnag/omc"
-  "golangci/golangci-lint"
-  "hairyhenderson/gomplate"
-  "homeport/dyff"
-  "twpayne/chezmoi"
-  "wagoodman/dive"
+declare -A repos=(
+  ["mikefarah/yq"]="yq_linux_$(get-arch --amd64 --arm64)"
+  ["ankitpokhrel/jira-cli"]="jira"
+  ["block/goose"]=""
+  ["coreos/ignition"]=""
+  ["coreos/butane"]=""
+  ["derailed/k9s"]=""
+  ["ericchiang/pup"]=""
+  ["getantibody/antibody"]=""
+  ["gmeghnag/omc"]="omc"
+  ["golangci/golangci-lint"]=""
+  ["hairyhenderson/gomplate"]=""
+  ["homeport/dyff"]=""
+  ["twpayne/chezmoi"]=""
+  ["wagoodman/dive"]=""
 )
 
 root_path="/out"
 
-mkdir -p "$root_path/yq"
-dra download -a mikefarah/yq -o "$root_path/yq/yq" --install-file "yq_linux_$(get-arch --amd64 --arm64)"
-
-for repo in "${repos[@]}"; do
+for repo in "${!repos[@]}"; do
+  # Determine the name of the executable/directory
   name="$(basename "$repo")"
+
+  # Determine the value for --install-file
+  install_file_val="${repos[$repo]}"
+  if [[ -z "$install_file_val" ]]; then
+    # If the value is empty, use basename for the --install-file
+    install_file_val="$name"
+  fi
+
   mkdir -p "$root_path/$name"
-  dra download -a "$repo" -o "$root_path/$name/$name" --install-file "$name"
+  # Use the calculated install_file_val in the dra download command
+  dra download -a "$repo" -o "$root_path/$name/$name" --install-file "$install_file_val"
 done
 
 mv "$root_path/ignition/ignition" "$root_path/ignition/ignition-validate"
+mv "$root_path/jira-cli/jira-cli" "$root_path/jira-cli/jira"
 
 mkdir -p "$root_path/oc"
 cd "$root_path/oc"
@@ -36,3 +46,4 @@ curl -L "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable/opensh
 find "$root_path" -type f -executable -exec mv {} "/usr/local/bin/" \;
 rm -rf "$root_path"
 rm /usr/local/bin/get-arch
+rm /usr/local/bin/dra
