ee285e51c225312a21ed9c98979f8400a77999fc
Zack Zlotnik <zackzlotnik@gmail.com>
2026-04-28 22:24:53 -0400

    use dnf for packages

--- tools-fetcher/install.sh
+++ tools-fetcher/install.sh
@@ -3,15 +3,9 @@
 set -xeuo
 
 declare -A repos=(
-  ["mikefarah/yq"]="yq_linux_$(get-arch --amd64 --arm64)"
   ["ankitpokhrel/jira-cli"]="jira"
-  ["coreos/ignition"]=""
-  ["coreos/butane"]=""
   ["derailed/k9s"]=""
-  ["ericchiang/pup"]=""
   ["gmeghnag/omc"]="omc"
-  ["golangci/golangci-lint"]=""
-  ["hairyhenderson/gomplate"]=""
   ["homeport/dyff"]=""
   ["twpayne/chezmoi"]=""
   ["wagoodman/dive"]=""
@@ -35,7 +29,6 @@ for repo in "${!repos[@]}"; do
   dra download -a "$repo" -o "$root_path/$name/$name" --install-file "$install_file_val"
 done
 
-mv "$root_path/ignition/ignition" "$root_path/ignition/ignition-validate"
 mv "$root_path/jira-cli/jira-cli" "$root_path/jira-cli/jira"
 
 mkdir -p "$root_path/oc"
